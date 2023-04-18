1fd863738b08ddeea4717e2831bb3f5594c0c3bb
Josiel Borges <josielborges555@gmail.com>
2023-04-18 20:03:21 -0300

    feat: add a simple CustomException

---
+++ api/clientes/exceptions.py
@@ -0,0 +1,11 @@
+from rest_framework.exceptions import APIException
+
+
+class CustomException(APIException):
+    detail = None
+    status_code = None
+
+    def __init__(self, detail, code):
+        super().__init__(detail, code)
+        self.detail = detail
+        self.status_code = code
