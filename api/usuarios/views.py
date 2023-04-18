712346ca93f19c616ed39e529ef1d453f25d43aa
Josiel Borges <josielborges555@gmail.com>
2023-04-18 13:53:52 -0300

    feat: usuário base views

--- api/usuarios/views.py
+++ api/usuarios/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from .models import Usuario
+from .serializers import UsuarioSerializer
+from .permissions import (
+    ContaPertenceAoUsuarioOuUsuarioAdmin,
+    UsuarioAdminOuApenasLeitura,
+)
+
+
+class UsuarioView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication, UsuarioAdminOuApenasLeitura]
+
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+
+class UsuarioDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [ContaPertenceAoUsuarioOuUsuarioAdmin]
+
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
