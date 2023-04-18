73cc8ffee81f933202ce0484782302cd772d0b18
Josiel Borges <josielborges555@gmail.com>
2023-04-18 20:03:52 -0300

    fix: Cliente model more according to project

--- api/clientes/models.py
+++ api/clientes/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Cliente(models.Model):
-    name = models.CharField(max_length=128)
-    cpf = models.CharField(max_length=11)
-    family_income = models.DecimalField()
+    name = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=10)
+    family_income = models.DecimalField(decimal_places=2, max_digits=8, null=True)
     birth_date = models.DateField()
     date_joined = models.DateField(auto_now_add=True)
     user = models.ForeignKey(
-        "usuarios.usuario", related_name="clients", on_delete=models.SET_NULL
+        "usuarios.usuario", related_name="clients", on_delete=models.SET_NULL, null=True
     )
