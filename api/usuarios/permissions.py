dda2eedca66c67ec55d8aef07a8626d7f606c526
Josiel Borges <josielborges555@gmail.com>
2023-04-18 20:05:37 -0300

    fix: remove useless permissions

--- api/usuarios/permissions.py
+++ api/usuarios/permissions.py
@@ -3,25 +3,13 @@ from .models import Usuario
 from rest_framework.views import View
 
 
-class UsuarioAdmin(permissions.BasePermission):
-    def has_permission(self, request, view: View) -> bool:
-        return bool(request.user.is_authenticated and request.user.is_superuser)
-
-
-class UsuarioAdminOuApenasLeitura(permissions.BasePermission):
-    def has_permission(self, request, view: View) -> bool:
-        return bool(
-            request.user.is_authenticated
-            and request.user.is_superuser
-            or request.method in permissions.SAFE_METHODS
-        )
-
-
-class ContaPertenceAoUsuarioOuUsuarioAdmin(permissions.BasePermission):
+class ContaPertenceAoUsuario(permissions.BasePermission):
     def has_object_permission(self, request, view: View, obj: Usuario) -> bool:
+        return bool(request.user.is_authenticated and obj == request.user)
+
+
+class EstaAutenticadoOuPost(permissions.BasePermission):
+    def has_permission(self, request, view: View) -> bool:
         return bool(
-            request.user.is_authenticated
-            and obj == request.user
-            or request.user.is_authenticated
-            and request.user.is_superuser
+            request.user and request.user.is_authenticated or request.method == "POST"
         )
