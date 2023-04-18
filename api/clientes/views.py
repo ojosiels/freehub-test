75bcf56e6ff65073c4d59664849d9a6028f05629
Josiel Borges <josielborges555@gmail.com>
2023-04-18 20:04:53 -0300

    feat: add cliente views

--- api/clientes/views.py
+++ api/clientes/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from .models import Cliente
+from .serializers import ClienteSerializer
+
+
+class ClienteView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
+
+    def filter_queryset(self, queryset):
+        client_name = self.request.query_params.get("name", None)
+
+        if client_name:
+            return queryset.filter(name__contains=client_name)
+        else:
+            return queryset
+
+
+class ClienteDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
