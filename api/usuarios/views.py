c570407791f7e78dd1af53e84e404e9f79cba4c2
Josiel Borges <josielborges555@gmail.com>
2023-04-18 20:06:45 -0300

    fix: changed user permission_classes

--- api/usuarios/views.py
+++ api/usuarios/views.py
@@ -1,16 +1,15 @@
 from rest_framework import generics
+
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Usuario
 from .serializers import UsuarioSerializer
-from .permissions import (
-    ContaPertenceAoUsuarioOuUsuarioAdmin,
-    UsuarioAdminOuApenasLeitura,
-)
+from .permissions import ContaPertenceAoUsuario, EstaAutenticadoOuPost
 
 
 class UsuarioView(generics.ListCreateAPIView):
-    authentication_classes = [JWTAuthentication, UsuarioAdminOuApenasLeitura]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [EstaAutenticadoOuPost]
 
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
@@ -18,7 +17,7 @@ class UsuarioView(generics.ListCreateAPIView):
 
 class UsuarioDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [ContaPertenceAoUsuarioOuUsuarioAdmin]
+    permission_classes = [ContaPertenceAoUsuario]
 
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
