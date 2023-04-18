70d6dd99af348fcb008673f7eb05ccad9f932f36
Josiel Borges <josielborges555@gmail.com>
2023-04-18 20:05:19 -0300

    fix: change Usuario model name max_length

--- api/usuarios/models.py
+++ api/usuarios/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 
 class Usuario(AbstractUser):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=150)
     email = models.EmailField(unique=True, max_length=50)
