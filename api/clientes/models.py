137ebd234e86a81654dd9fca3d16dfbddc943637
Josiel Borges <josielborges555@gmail.com>
2023-04-18 11:37:30 -0300

    feat: cliente base model

--- api/clientes/models.py
+++ api/clientes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    name = models.CharField(max_length=128)
+    cpf = models.CharField(max_length=11)
+    family_income = models.DecimalField()
+    birth_date = models.DateField()
+    date_joined = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(
+        "usuarios.usuario", related_name="clients", on_delete=models.SET_NULL
+    )
