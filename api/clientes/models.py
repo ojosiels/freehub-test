410571467897f8677505b6554e6ad95bbc4df1c2
Josiel Borges <josielborges555@gmail.com>
2023-04-22 14:34:29 -0300

    fix: change client cpf length

--- api/clientes/models.py
+++ api/clientes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Cliente(models.Model):
     name = models.CharField(max_length=150)
-    cpf = models.CharField(max_length=10)
+    cpf = models.CharField(max_length=11)
     family_income = models.DecimalField(decimal_places=2, max_digits=8, null=True)
     birth_date = models.DateField()
     date_joined = models.DateField(auto_now_add=True)
