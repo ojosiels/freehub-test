9f159ac2ecf747fa22192fa4a0f51b5c0eec2c50
Josiel Borges <josielborges555@gmail.com>
2023-04-24 00:33:28 -0300

    feat: code identation

--- api/api/settings.py
+++ api/api/settings.py
@@ -18,7 +18,7 @@ from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY =  get_random_secret_key()
+SECRET_KEY = get_random_secret_key()
 
 DEBUG = True
 
