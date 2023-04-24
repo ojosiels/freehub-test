15ff32f2eac43e6d8edb7c1f7797c23c9705943d
Josiel Borges <josielborges555@gmail.com>
2023-04-23 21:20:14 -0300

    feat: routes index

--- webpage/src/routes/index.tsx
+++ webpage/src/routes/index.tsx
@@ -1,14 +1,20 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import MainPage from "../pages/MainPage";
+import ListClientsPage from "../pages/ListClientsPage";
 
-import Header from "../components/Header";
+import ProtectedRoutes from "../components/ProtectedRoutes";
 
-const RoutesMain = () => {
+const RoutesMain = (): JSX.Element => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/main" element={<MainPage />} />
+
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/clients" element={<ListClientsPage />} />
+      </Route>
+
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
