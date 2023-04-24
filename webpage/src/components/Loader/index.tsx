93e700c12b53ecf1cae908eebc4a1df21f4879dc
Josiel Borges <josielborges555@gmail.com>
2023-04-23 21:16:13 -0300

    feat: loading screen

---
+++ webpage/src/components/Loader/index.tsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <h1>Carregando</h1>
+      <span className="bullets">.</span>
+    </div>
+  );
+};
+
+export default Loader;
